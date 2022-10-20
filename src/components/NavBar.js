a99d27edeb177fa16a2491f7bd511fa88270096c
Vicky Phung <vphung@perscholas.org>
2022-10-20 19:58:58 -0400

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBar (props) {
+  return (
+    <nav>
+      <h3>{props?.user?.name}</h3>
+      <Link to="/orders">Order History</Link>&nbsp;
+      <Link to="/orders/new">New Order</Link>
+      <button onClick={() => {
+        localStorage.removeItem('token');
+        props.setUser(null);
+      }}>Log Out!</button>
+    </nav>
+  )
+}
